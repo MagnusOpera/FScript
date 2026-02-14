80708d2da06c102f233a9968bd93b01f39e15847
Pierre Chalamet <pierre@magnusopera.io>
2026-02-14 21:15:33 +0100

    Delete old LanguageServerLegacy directory

--- src/FScript.CSharpInterop/LanguageServerLegacy/LspRuntimeExterns.fs
+++
@@ -1,8 +0,0 @@
-namespace FScript.LanguageServer
-
-open FScript.Language
-open FScript.CSharpInterop
-
-module LspRuntimeExterns =
-    let forSourcePath (sourcePath: string) : ExternalFunction list =
-        InteropServices.runtimeExternsForSourcePath sourcePath
